6a88c0449c12638633f8973be1f446cb0e870798
Salma <sahmad.salma@gmail.com>
2024-05-15 16:31:21 +0100

    image uploaded

--- app/src/main/java/com/example/l3ezlaapp/Adapter/ImageAdapter.kt
+++ app/src/main/java/com/example/l3ezlaapp/Adapter/ImageAdapter.kt
@@ -1,47 +1,28 @@
-package com.example.l3ezlaapp.Adapter
-
-import android.content.Context
 import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.widget.ViewPager2
 import com.example.l3ezlaapp.R
 
-class ImageAdapter(
-    private var sliderItems: List<Bitmap>,
-    private val viewPager2: ViewPager2
-) : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
-
-    private lateinit var context: Context
-    private val runnable = Runnable {
-        sliderItems = sliderItems
-        notifyDataSetChanged()
-    }
+class ImageAdapter(private val images: List<Bitmap>) : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
-        context = parent.context
-        val view = LayoutInflater.from(context)
-            .inflate(R.layout.slider_item_container, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
         return ImageViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
-        holder.setImage(sliderItems[position])
-        if (position == sliderItems.size - 2) {
-            viewPager2.post(runnable)
-        }
+        val bitmap = images[position]
+        holder.imageView.setImageBitmap(bitmap)
     }
 
-    override fun getItemCount(): Int = sliderItems.size
+    override fun getItemCount(): Int {
+        return images.size
+    }
 
     class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val imageView: ImageView = itemView.findViewById(R.id.imageSlide)
-
-        fun setImage(bitmap: Bitmap) {
-            imageView.setImageBitmap(bitmap)
-        }
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
     }
 }
